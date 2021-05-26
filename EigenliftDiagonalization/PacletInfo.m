ae6dc8b3899f1bc5d04e8fe60f63a428651bcc17
sarahkfriday <fridays@southwestern.edu>
2021-05-26 17:09:32 -0500

    created workbench project

---
+++ EigenliftDiagonalization/PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2021.05.26*)
+
+Paclet[
+  Name -> "EigenliftVisualizer",
+  Version -> "0.0.1",
+  WolframVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
